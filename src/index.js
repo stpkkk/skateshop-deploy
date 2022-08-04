e14df2dba48f82fcde0635558c3df8cfbb691626
Stepanov Igor <stipyk1309@gmail.com>
2022-08-04 17:11:08 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App"
 import "./index.scss";
 import "macro-css"; //библиотека кастомных наборов стилей по типу bootsrap документация тут https://github.com/Archakov06/macro-css
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
